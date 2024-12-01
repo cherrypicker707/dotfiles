4b2911a4c4c99be8a4989b36d295211f69f5fe16
Wiktor Marczak <cherrypicker707@proton.me>
2024-12-01 12:07:09 +0100

    Configuration for fish added.

--- copy.fish
+++ copy.fish
@@ -1,3 +1,5 @@
 # Script for Copying Files into This Repo
-cp -r ~/.config/i3 .config/
-cp -r ~/.config/alacritty .config/
+cp -r ~/.config/i3/config .config/i3/config
+cp -r ~/.config/alacritty/alacritty.toml .config/alacritty/alacritty.toml
+cp -r ~/.config/fish/config.fish .config/fish/config.fish
+cp -r ~/.config/fish/functions/fish_prompt.fish .config/fish/functions/fish_prompt.fish
