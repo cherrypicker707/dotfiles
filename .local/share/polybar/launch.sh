fe7bfa2c6800205ed7032b10fe27b4bdfa36651b
Wiktor Marczak <cherrypicker707@proton.me>
2024-12-01 12:16:28 +0100

    Configuration for Polybar added. Small fixes in configuration for i3.

---
+++ .local/share/polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Script for Launching Polybar
+
+polybar-msg cmd quit
+echo "---" | tee -a /tmp/polybar.log
+polybar bar 2>&1 | tee -a /tmp/polybar.log & disown
+echo "Bar launched..."
--- copy.fish
+++ copy.fish
@@ -1,6 +1,8 @@
 # Script for Copying Files into This Repo
-cp -r ~/.config/i3/config .config/i3/config
-cp -r ~/.config/alacritty/alacritty.toml .config/alacritty/alacritty.toml
-cp -r ~/.config/fish/config.fish .config/fish/config.fish
-cp -r ~/.config/fish/functions/fish_prompt.fish .config/fish/functions/fish_prompt.fish
-cp -r ~/.config/picom/picom.conf .config/picom/picom.conf
+cp ~/.config/i3/config .config/i3/config
+cp ~/.config/alacritty/alacritty.toml .config/alacritty/alacritty.toml
+cp ~/.config/fish/config.fish .config/fish/config.fish
+cp ~/.config/fish/functions/fish_prompt.fish .config/fish/functions/fish_prompt.fish
+cp ~/.config/picom/picom.conf .config/picom/picom.conf
+cp ~/.config/polybar/config.ini .config/polybar/config.ini
+cp ~/.local/share/polybar/launch.sh .local/share/polybar/launch.sh
