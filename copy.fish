d2edb6b521d1fa39739a54a0f3ec25a04c93060a
Wiktor Marczak <cherrypicker707@proton.me>
2024-12-01 12:10:06 +0100

    Configuration for picom added.

--- copy.fish
+++ copy.fish
@@ -3,3 +3,4 @@ cp -r ~/.config/i3/config .config/i3/config
 cp -r ~/.config/alacritty/alacritty.toml .config/alacritty/alacritty.toml
 cp -r ~/.config/fish/config.fish .config/fish/config.fish
 cp -r ~/.config/fish/functions/fish_prompt.fish .config/fish/functions/fish_prompt.fish
+cp -r ~/.config/picom/picom.conf .config/picom/picom.conf
