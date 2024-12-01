183ce190a9808fbd0e18322af0cd1f73479957d5
Wiktor Marczak <cherrypicker707@proton.me>
2024-12-01 12:20:18 +0100

    Configuration for Rofi added.

--- copy.fish
+++ copy.fish
@@ -6,3 +6,5 @@ cp ~/.config/fish/functions/fish_prompt.fish .config/fish/functions/fish_prompt.
 cp ~/.config/picom/picom.conf .config/picom/picom.conf
 cp ~/.config/polybar/config.ini .config/polybar/config.ini
 cp ~/.local/share/polybar/launch.sh .local/share/polybar/launch.sh
+cp ~/.config/rofi/config.rasi .config/rofi/config.rasi
+cp ~/.local/share/rofi/theme.rasi .local/share/rofi/theme.rasi
