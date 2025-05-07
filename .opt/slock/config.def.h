5a0915c7518ab15685e0a236ba6b21da6fa5840a
Wiktor Marczak <cherrypicker707@proton.me>
2025-05-07 08:34:30 +0200

    Configuration for slock added.

---
+++ .opt/slock/config.def.h
@@ -0,0 +1,13 @@
+// Configuration for slock
+// Used during compilation.
+
+static const char *user  = "cherrypicker707";
+static const char *group = "wheel";
+
+static const char *colorname[NUMCOLS] = {
+    [INIT] =   "#1e1e2e",
+    [INPUT] =  "#94e2d5",
+    [FAILED] = "#f38ba8",
+};
+
+static const int failonclear = 0;
